bad5d24f2c884ff6ad99c48d9f116f13396ca5ff
xuhuan <xuhuanxxx@gmail.com>
2020-03-17 04:19:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,11 +8,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-sed -i '/list listen_https [::]:443/d' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/list listen_https 0.0.0.0:443/d' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/HTTP listen addresses/a\        list listen_https [::]:443' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/HTTP listen addresses/a\        list listen_https 0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/list listen_https [::]:443/d' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/list listen_https 0.0.0.0:443/d' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/HTTP listen addresses/a\        list listen_https [::]:443' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/HTTP listen addresses/a\        list listen_https 0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
 
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/vernesong/OpenClash package/OpenClash
