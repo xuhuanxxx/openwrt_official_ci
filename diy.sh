b46b913770a87218b3871d2de27a2631b47a13e2
xuhuan <xuhuanxxx@gmail.com>
2020-03-16 22:07:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,17 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+sed -i '/list listen_https [::]:443/d' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/list listen_https 0.0.0.0:443/d' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/HTTP listen addresses/a\        list listen_https [::]:443' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/HTTP listen addresses/a\        list listen_https 0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
+
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+git clone https://github.com/falafalafala1668/OpenWrt-UEFI-Support OpenWrt-UEFI-Support
+./OpenWrt-UEFI-Support/RunMe.sh apply
