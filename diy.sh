2d634c9b71f73d97f1d3664f3c3a5d65fa015fb5
xuhuan <xuhuanxxx@gmail.com>
2020-03-17 22:48:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,13 +6,29 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i '/root/d' package/base-files/files/etc/shadow
+sed -i '1i\root:$1$XVyBXUIp$qDem8k6G0pkLNUCOB9f0G/:18334:0:99999:7:::'  package/base-files/files/etc/shadow
 
-sed -i '/list listen_https [::]:443/d' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/list listen_https 0.0.0.0:443/d' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/HTTP listen addresses/a\        list listen_https [::]:443' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/HTTP listen addresses/a\        list listen_https 0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
+sed -i 's/option lang auto/option lang "zh_cn"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i '/option Bootstrap/d' feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i '/option Material/d' feeds/luci/modules/luci-base/root/etc/config/luci
+
+sed -i '/config internal themes/a\        option Material "/luci-static/material"' feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i '/config internal themes/a\        option Bootstrap "/luci-static/bootstrap"' feeds/luci/modules/luci-base/root/etc/config/luci
+
+
+
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i '/timezone/d' package/base-files/files/bin/config_generate
+sed -i '/zonename/d' package/base-files/files/bin/config_generate
+sed -i "/hostname/a\                set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+sed -i "/hostname/a\                set system.@system[-1].timezone='CST-8'" package/base-files/files/bin/config_generate
+
+sed -i '/listen_https/d' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/HTTP listen addresses/a\        list listen_https       [::]:443' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/HTTP listen addresses/a\        list listen_https       0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
 sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
+
 
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/vernesong/OpenClash package/OpenClash
