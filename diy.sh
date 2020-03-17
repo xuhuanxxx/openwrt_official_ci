0c0e737e45ee0bd83e1a4fff7910375214b22edb
xuhuan <xuhuanxxx@gmail.com>
2020-03-18 01:25:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,11 +10,20 @@ sed -i '/root/d' package/base-files/files/etc/shadow
 sed -i '1i\root:$1$XVyBXUIp$qDem8k6G0pkLNUCOB9f0G/:18334:0:99999:7:::'  package/base-files/files/etc/shadow
 
 sed -i 's/option lang auto/option lang "zh_cn"/g' feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i '/option Bootstrap/d' feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i '/option Material/d' feeds/luci/modules/luci-base/root/etc/config/luci
 
-sed -i '/config internal themes/a\        option Material "/luci-static/material"' feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i '/config internal themes/a\        option Bootstrap "/luci-static/bootstrap"' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/netgear/d' feeds.conf.default
+sed -i '$a\src-git netgear git://github.com/ysoyipek/luci-theme-netgear.git' feeds.conf.default
+
+# sed -i '/https://github.com/rosywrt/luci.git/d' feeds.conf.default
+sed -i '$a\src-git luci https://github.com/rosywrt/luci.git' feeds.conf.default
+
+# sed -i '/option Netgear/d' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/option Bootstrap/d' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/option Material/d' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# sed -i '/config internal themes/a\        option Netgear "/luci-static/netgear"' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/config internal themes/a\        option Material "/luci-static/material"' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/config internal themes/a\        option Bootstrap "/luci-static/bootstrap"' feeds/luci/modules/luci-base/root/etc/config/luci
 
 
 
@@ -30,9 +39,29 @@ sed -i '/HTTP listen addresses/a\        list listen_https       0.0.0.0:443' pa
 sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
 
 
+if [ -d "package/luci-app-clash" ]; then
+	rm -rf "package/luci-app-clash"
+fi
+
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+
+if [ -d "package/OpenClash" ]; then
+	rm -rf "package/OpenClash"
+fi
+
 git clone https://github.com/vernesong/OpenClash package/OpenClash
+
+if [ -d "package/luci-app-adguardhome" ]; then
+	rm -rf "package/luci-app-adguardhome"
+fi
+
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+if [ -d "package/luci-app-filetransfer" ]; then
+	rm -rf "package/luci-app-filetransfer"
+fi
+
+git clone https://github.com/f8q8/luci-app-filetransfer package/luci-app-filetransfer
 
 git clone https://github.com/falafalafala1668/OpenWrt-UEFI-Support OpenWrt-UEFI-Support
 ./OpenWrt-UEFI-Support/RunMe.sh apply
