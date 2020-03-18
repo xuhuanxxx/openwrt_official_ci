1e788ea3b60a11a51fee488fddae79ab8999fd8e
xuhuan <xuhuanxxx@gmail.com>
2020-03-19 00:24:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,19 +11,6 @@ sed -i '1i\root:$1$XVyBXUIp$qDem8k6G0pkLNUCOB9f0G/:18334:0:99999:7:::'  package/
 
 sed -i 's/option lang auto/option lang "zh_cn"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-# sed -i '/netgear/d' feeds.conf.default
-sed -i '$a\src-git netgear git://github.com/ysoyipek/luci-theme-netgear.git' feeds.conf.default
-
-# sed -i '/option Netgear/d' feeds/luci/modules/luci-base/root/etc/config/luci
-# sed -i '/option Bootstrap/d' feeds/luci/modules/luci-base/root/etc/config/luci
-# sed -i '/option Material/d' feeds/luci/modules/luci-base/root/etc/config/luci
-
-# sed -i '/config internal themes/a\        option Netgear "/luci-static/netgear"' feeds/luci/modules/luci-base/root/etc/config/luci
-# sed -i '/config internal themes/a\        option Material "/luci-static/material"' feeds/luci/modules/luci-base/root/etc/config/luci
-# sed -i '/config internal themes/a\        option Bootstrap "/luci-static/bootstrap"' feeds/luci/modules/luci-base/root/etc/config/luci
-
-
-
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i '/timezone/d' package/base-files/files/bin/config_generate
 sed -i '/zonename/d' package/base-files/files/bin/config_generate
@@ -33,7 +20,7 @@ sed -i "/hostname/a\                set system.@system[-1].timezone='CST-8'" pac
 sed -i '/listen_https/d' package/network/services/uhttpd/files/uhttpd.config
 sed -i '/HTTP listen addresses/a\        list listen_https       [::]:443' package/network/services/uhttpd/files/uhttpd.config
 sed -i '/HTTP listen addresses/a\        list listen_https       0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
 
 
 if [ -d "package/luci-app-clash" ]; then
@@ -54,11 +41,17 @@ fi
 
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
-if [ -d "package/luci-app-filetransfer" ]; then
-	rm -rf "package/luci-app-filetransfer"
+
+if [ -d "package/luci-theme-argon" ]; then
+	        rm -rf "package/luci-theme-argon"
 fi
 
-git clone https://github.com/f8q8/luci-app-filetransfer package/luci-app-filetransfer
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+
+if [ -d "OpenWrt-UEFI-Support" ]; then
+	rm -rf "OpenWrt-UEFI-Support"
+fi
 
 git clone https://github.com/falafalafala1668/OpenWrt-UEFI-Support OpenWrt-UEFI-Support
 ./OpenWrt-UEFI-Support/RunMe.sh apply
