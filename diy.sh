0c278bdecc0a575cd40572262fdcc67a11a0cb46
xuhuan <xuhuanxxx@gmail.com>
2020-03-26 10:40:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,27 +9,14 @@ sed -i '/zonename/d' package/base-files/files/bin/config_generate
 sed -i "/hostname/a\                set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 sed -i "/hostname/a\                set system.@system[-1].timezone='CST-8'" package/base-files/files/bin/config_generate
 
-sed -i '/listen_https/d' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/HTTP listen addresses/a\        list listen_https       [::]:443' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/HTTP listen addresses/a\        list listen_https       0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
-sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
-
-if [ -d "package/luci-theme-argon" ]; then
-	rm -rf "package/luci-theme-argon"
-fi
+# sed -i '/listen_https/d' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/HTTP listen addresses/a\        list listen_https       [::]:443' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/HTTP listen addresses/a\        list listen_https       0.0.0.0:443' package/network/services/uhttpd/files/uhttpd.config
+# sed -i '/redirect_https/s/0/1/g' package/network/services/uhttpd/files/uhttpd.config
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-
-if [ -d "package/luci-app-clash" ]; then
-	rm -rf "package/luci-app-clash"
-fi
-
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
-
-if [ -d "OpenWrt-UEFI-Support" ]; then
-	rm -rf "OpenWrt-UEFI-Support"
-fi
 
 git clone https://github.com/falafalafala1668/OpenWrt-UEFI-Support OpenWrt-UEFI-Support
 ./OpenWrt-UEFI-Support/RunMe.sh apply
